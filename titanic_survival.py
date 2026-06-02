b7632ce4f06bcee16a0401d83352f0084404b5ba
Lohita-08 <25cb028@rmkec.ac.in>
2026-06-02 20:00:11 +0530

    Initial commit - Titanic ML project

---
+++ titanic_survival.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+df = pd.read_csv("Titanic-Dataset.csv")
+
+# Data Cleaning
+df['Age'] = df['Age'].fillna(df['Age'].median())
+df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
+df.drop('Cabin', axis=1, inplace=True)
+
+# Convert text to numbers
+le = LabelEncoder()
+df['Sex'] = le.fit_transform(df['Sex'])
+df['Embarked'] = le.fit_transform(df['Embarked'])
+
+# Select Features (X) and Target (y)
+X = df[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
+y = df['Survived']
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+cm = confusion_matrix(y_test, y_pred)
+
+print("Confusion Matrix:")
+print(cm)
+
+plt.imshow(cm)
+plt.title("Confusion Matrix")
+plt.colorbar()
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.show()
+
+plt.savefig("confusion_matrix.png")
+plt.show()
